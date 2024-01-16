0b4ba14d068468d23d1d5274fec47954e04ddf7b
khaleed2002 <zekomadred159@gmail.com>
2024-01-16 16:49:15 +0200

    update text

--- src/app.js
+++ src/app.js
@@ -47,7 +47,7 @@ app.get('/help', (req, res) => {
     res.render('help', {
         title: "Help",
         name: "Khaled",
-        message: "Error occured"
+        message: "Help message"
     })
 })
 
