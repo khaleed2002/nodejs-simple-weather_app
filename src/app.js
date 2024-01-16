3ac08eb9b74f89bd5c0a0d0079f4e54b2e73efb6
khaleed2002 <zekomadred159@gmail.com>
2024-01-16 16:10:28 +0200

    update port log

--- src/app.js
+++ src/app.js
@@ -110,6 +110,6 @@ app.get('*', (req, res) => {
     })
 })
 app.listen(port, () => {
-    console.log("Server is listening on port 3000.");
+    console.log(`Server is listening on port ${port}.`);
 })
 
